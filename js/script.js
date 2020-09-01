3607f24f226dd955b788df87e952a02b779427fe
vanshika-1211 <vanshika1910202@akgec.ac.in>
2020-09-01 19:22:30 +0530

    added function for score and life update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-var life = 5;
-var lifeSpan=document.getElementById('life')
-lifeSpan.textContent=life;
-
 function start()
 {
+	var life = 5;
+	var score=0;
+	var id2,id2,id3;
+	var scoreSpan=document.getElementById('score');
+	var lifeSpan=document.getElementById('life');
+	scoreSpan.textContent=score;
+	lifeSpan.textContent=life;
+	var obj=document.getElementById("basket");
+	var obj1=document.getElementById("egg1");
+	var pos1=0;
+	var obj2=document.getElementById("egg2");
+	var pos2=0;
+	var obj3=document.getElementById("egg3");
+	var pos3=0;
+	var base=document.getElementById('base');
+	var restart=document.getElementById('restart');
 	/*function for moving basket using keyboard*/
-	var obj = document.getElementById("basket");
 	document.addEventListener('keydown', moveSelection);
 	function moveSelection(event)
 		{
@@ -19,52 +30,81 @@ function start()
 		    }
 		 }
 
-/*function for falling eggs rendomly*/
-   var obj1 = document.getElementById("egg1");
-   var pos1= 0;
-   setInterval(fall_egg1,5)
-   function fall_egg1()
-   {
-    if(pos1==380)
-    {
-    	pos1=0;
-    }
-    else
-    {
-    	pos1++;
-        obj1.style.top=pos1 +"px";
-    }
+  /*function for falling eggs rendomly*/
+  id1=setInterval(fall_egg1,6)
+  function fall_egg1() {
+     if(pos1==450) {
+			 	 check_collision(obj1,obj);
+      	 pos1=0;
+     }
+     else {
+      	 pos1++;
+         obj1.style.top=pos1 +"px";
+
+     }
+  }
+
+  id2=setInterval(fall_egg2,8)
+  function fall_egg2()
+  {
+     if(pos2==450)
+     {
+			 	check_collision(obj2,obj);
+      	 pos2=0;
+     }
+     else
+     {
+      	 pos2++;
+         obj2.style.top=pos2 +"px";
+     }
+  }
+
+  id3=setInterval(fall_egg3,9)
+  function fall_egg3()
+  {
+     if(pos3==450)
+     {
+			 		check_collision(obj3,obj);
+        	pos3=0;
+     }
+     else
+     {
+        	pos3++;
+          obj3.style.top=pos3 +"px";
+     }
+  }
+
+
+  function check_collision(div1, div2){
+			var x1=div1.offsetLeft;
+			var y1=div1.offsetTop;
+			var h1=div1.offsetHeight;
+			var w1=div1.offsetWidth;
+			var b1 = y1+h1;
+			var r1 = x1+w1;
+			var x2=div2.offsetLeft;
+			var h2=div2.offsetHeight;
+			var y2=div2.offsetTop;
+			var w2=div2.offsetWidth;
+			var b2 = y2+h2;
+			var r2 = x2+w2;
+			if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2){
+				 life--;
+				 lifeSpan.textContent=life;
+				 stop_game();
+			}
+	    else{
+				score++;
+				scoreSpan.textContent=score;
+			}
    }
 
-   var obj2 = document.getElementById("egg2");
-   var pos2=0;
-   setInterval(fall_egg2,4)
-   function fall_egg2()
-   {
-    if(pos2==380)
-    {
-    	pos2=0;
-    }
-    else
-    {
-    	pos2++;
-        obj2.style.top=pos2 +"px";
-    }
-   }
-
-   var obj3 = document.getElementById("egg3");
-   var pos3=0;
-   setInterval(fall_egg3,6)
-   function fall_egg3()
-   {
-    if(pos3==380)
-    {
-    	pos3=0;
-    }
-    else
-    {
-    	pos3++;
-        obj3.style.top=pos3 +"px";
-    }
-   }
+	 function stop_game() {
+		 if(life==0){
+			 clearInterval(id1);
+			 clearInterval(id2);
+			 clearInterval(id3);
+			 restart.style.display='block';
+		 }
+	 }
 }
